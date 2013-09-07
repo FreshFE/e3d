c1176d6ae6535d012412b85e851d0b7bc6ffe881
MinoWu <minowu@foxmail.com>
2013-09-07 16:00:34 +0800

    完成Home页面第一部分功能

--- asset/win8/lib/script.js
+++ asset/win8/lib/script.js
@@ -12,3 +12,26 @@ $(document).ready(function() {
 	$('#place').css({'left': left, 'top': top});
 });
 
+
+/**
+ * Angular JS
+ */
+var app = angular.module('App', []);
+
+app.controller('HomeController', function($scope, $http) {
+
+	$scope.items = [];
+
+	$scope.fetch = function() {
+		$http({
+			method: "GET",
+			url: 'cases/cover/cover.json'
+		}).success(function(data, status, headers, config) {
+
+			$scope.items = data;
+		});
+	};
+
+	$scope.fetch();
+
+});
